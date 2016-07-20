e261e67f778c5adca671cc203bb244edf532765c
Lars Moelleken <lmoelleken@menadwork.com>
2016-07-20 12:37:52 +0200

    [+]: use "assertSame" instead of "assertEquals"

--- tests/Test/BounceMailHandler/MultipleMessageTest.php
+++ tests/Test/BounceMailHandler/MultipleMessageTest.php
@@ -44,7 +44,7 @@ class MultipleMessageTest extends \PHPUnit_Framework_TestCase
       $rc = $bmh->openLocal($this->getMailboxPath(__DIR__ . '/../../fixtures/' . $testFile));
       ob_end_clean();
 
-      $this->assertTrue($rc, $testFile . ': openLocal');
+      self::assertTrue($rc, $testFile . ': openLocal');
 
       $bmh->actionFunction =
           function ($msgnum, $bounceType, $email, $subject, $xheader, $remove, $ruleNo, $ruleCat, $totalFetched, $body) use ($expected) {
@@ -56,22 +56,22 @@ class MultipleMessageTest extends \PHPUnit_Framework_TestCase
       $output = ob_get_contents();
       ob_end_clean();
 
-      $this->assertTrue($rc, $testFile . ': processMailbox');
+      self::assertTrue($rc, $testFile . ': processMailbox');
 
       preg_match('/Read: ([0-9]+) messages/', $output, $matches);
-      $this->assertEquals($fetched, $matches[1], $testFile . ': messages read');
+      self::assertSame($fetched, $matches[1], $testFile . ': messages read');
 
       preg_match('/([0-9]+) action taken/', $output, $matches);
-      $this->assertEquals($processed, $matches[1], $testFile . ': action taken');
+      self::assertSame($processed, $matches[1], $testFile . ': action taken');
 
       preg_match('/([0-9]+) no action taken/', $output, $matches);
-      $this->assertEquals($unprocessed, $matches[1], $testFile . ': no action taken');
+      self::assertSame($unprocessed, $matches[1], $testFile . ': no action taken');
 
       preg_match('/([0-9]+) messages deleted/', $output, $matches);
-      $this->assertEquals($deleted, $matches[1], $testFile . ': messages deleted');
+      self::assertSame($deleted, $matches[1], $testFile . ': messages deleted');
 
       preg_match('/([0-9]+) messages moved/', $output, $matches);
-      $this->assertEquals($moved, $matches[1], $testFile . ': messages moved');
+      self::assertSame($moved, $matches[1], $testFile . ': messages moved');
     }
   }
 
--- tests/Test/BounceMailHandler/SingleMessageTest.php
+++ tests/Test/BounceMailHandler/SingleMessageTest.php
@@ -1430,7 +1430,7 @@ class SingleMessageTest extends \PHPUnit_Framework_TestCase
       $rc = $bmh->openLocal($this->getMailboxPath(__DIR__ . '/../../fixtures/' . $testFile));
       ob_end_clean();
 
-      $this->assertTrue($rc, $testFile . ': openLocal');
+      self::assertTrue($rc, $testFile . ': openLocal');
 
       $bmh->actionFunction =
           function ($msgnum, $bounceType, $email, $subject, $xheader, $remove, $ruleNo, $ruleCat, $totalFetched, $body) use ($self, $testFile, $expected) {
@@ -1442,28 +1442,28 @@ class SingleMessageTest extends \PHPUnit_Framework_TestCase
       $output = ob_get_contents();
       ob_end_clean();
 
-      $this->assertTrue($rc, $testFile . ': processMailbox');
+      self::assertTrue($rc, $testFile . ': processMailbox');
 
       preg_match('/Read: ([0-9]+) messages/', $output, $matches);
-      $this->assertEquals($fetched, $matches[1], $testFile . ': messages read');
+      self::assertSame($fetched, $matches[1], $testFile . ': messages read');
 
       preg_match('/([0-9]+) action taken/', $output, $matches);
-      $this->assertEquals($processed, $matches[1], $testFile . ': action taken');
+      self::assertSame($processed, $matches[1], $testFile . ': action taken');
 
       preg_match('/([0-9]+) no action taken/', $output, $matches);
-      $this->assertEquals($unprocessed, $matches[1], $testFile . ': no action taken');
+      self::assertSame($unprocessed, $matches[1], $testFile . ': no action taken');
 
       preg_match('/([0-9]+) messages deleted/', $output, $matches);
-      $this->assertEquals($deleted, $matches[1], $testFile . ': messages deleted');
+      self::assertSame($deleted, $matches[1], $testFile . ': messages deleted');
 
       preg_match('/([0-9]+) messages moved/', $output, $matches);
-      $this->assertEquals($moved, $matches[1], $testFile . ': messages moved');
+      self::assertSame($moved, $matches[1], $testFile . ': messages moved');
 
       preg_match('/Match: ([^:]+):([^;]+); ([^;]*); <?([^<]*)/', $output, $matches);
-      $this->assertEquals($ruleNo, $matches[1], $testFile . ': rule_no');
-      $this->assertEquals($ruleCat, $matches[2], $testFile . ': rule_cat');
-      $this->assertEquals($bounceType, $matches[3], $testFile . ': bounce_type');
-      $this->assertEquals($email, $matches[4], $testFile . ': email');
+      self::assertSame($ruleNo, $matches[1], $testFile . ': rule_no');
+      self::assertSame($ruleCat, $matches[2], $testFile . ': rule_cat');
+      self::assertSame($bounceType, $matches[3], $testFile . ': bounce_type');
+      self::assertSame($email, $matches[4], $testFile . ': email');
     }
   }
 
