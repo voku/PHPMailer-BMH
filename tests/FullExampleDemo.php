19aa7c4927b1175d940f4e19f429dbf57d7d3299
Lars Moelleken <lars@moelleken.org>
2016-11-13 03:07:56 +0100

    [+]: try to add live-tests via imap (Dovecot) v2.7.6
    -> need some testing via Travis CI

--- tests/FullExampleDemo.php
+++ tests/FullExampleDemo.php
@@ -51,9 +51,9 @@ class FullExampleDemo
     $bmh->mailhost = '127.0.0.1'; // your mail server
     $bmh->mailboxUserName = 'testuser'; // your mailbox username
     $bmh->mailboxPassword = 'applesauce!'; // your mailbox password
-    $bmh->port = 143; // the port to access your mailbox, default is 143
+    $bmh->port = 993; // the port to access your mailbox, default is 143
     $bmh->service = 'imap'; // the service to use (imap or pop3), default is 'imap'
-    $bmh->serviceOption = 'notls'; // the service options (none, tls, notls, ssl, etc.), default is 'notls'
+    $bmh->serviceOption = 'novalidate-cert'; // the service options (none, tls, notls, ssl, etc.), default is 'notls'
     $bmh->boxname = 'INBOX'; // the mailbox to access, default is 'INBOX'
 
     $bmh->moveHard           = true; // default is false
