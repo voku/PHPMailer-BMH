5f01689c2c987b599d29c6bc85ec8ef60447c3f4
Lars Moelleken <lars@moelleken.org>
2016-11-13 05:15:40 +0100

    [+]: try to fix test-results
    -> more work needed -> add more test-mails

--- tests/FullExampleDemoTest.php
+++ tests/FullExampleDemoTest.php
@@ -15,7 +15,10 @@ class FullExampleDemoTest extends \PHPUnit_Framework_TestCase
 
     // give us the debug-results
     // ----------------------------------------------------------
-    self::assertSame('todo', $bounce->getDebug());
+    self::assertSame('1: 0000 | unrecognized | none | <span style="color:gray;">not deleted</span> | Robert Hafner <tedium@gmail.com> | Are you looking for tedivm? Re: With CC<br />
+2: 0000 | unrecognized | none | <span style="color:gray;">not deleted</span> | tedivm@tedivm.com | Welcome<br />
+3: 0000 | unrecognized | none | <span style="color:gray;">not deleted</span> | Robert Hafner <tedivm@tedivm.com> | Formatted<br />
+', $bounce->getDebug());
 
   }
 }
